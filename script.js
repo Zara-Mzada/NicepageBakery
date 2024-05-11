c5d8a5933e1bd70f5d35953cddd1b4ddf0e62ab8
Zahra Malikzada <zara@Zahras-MacBook-Air.local>
2024-05-11 16:25:49 +0400

    Initial

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('.menu-button');
+const navbarOpening=document.querySelector('.nav-container');
+const closeNavbar = document.querySelector('.fa-xmark');
+
+
+function navOpenning (){
+    navbarOpening.style.display='block';
+}
+function navClosing (){
+    navbarOpening.style.display = 'none';
+}
